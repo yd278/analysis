e80366fc11a0d9a6026193016cac962706834b67
Terence Tao <tao@math.ucla.edu>
2025-06-13 20:09:59 -0700

    Finish Prop 7.1.8

--- analysis/Analysis/Section_7_1.lean
+++ analysis/Analysis/Section_7_1.lean
@@ -9,27 +9,159 @@ Technical note: it is convenient in Lean to extend finite sequences (usually by 
 
 Main constructions and results of this section:
 
--
+- API for summation over finite sets (encoded using Mathlib's `Finset` type), using the `Finset.sum` method and the `∑ n ∈ A, f n` notation.
 
 -/
 
+-- This makes available the convenient notation `∑ n ∈ A, f n` to denote summation of `f n` for `n` ranging over a finite set `A`.
 open BigOperators
 
+-- This is a technical device to avoid Mathlib's insistence on decidable equality for finite sets.
+open Classical
+
+namespace Finset
+
 -- We use `Finset.Icc` to describe finite intervals in the integers.  `Finset.mem_Icc` is the standard Mathlib tool for checking membership in such intervals.
-#check Finset.mem_Icc
+#check mem_Icc
 
 /-- Definition 7.1.1 -/
-theorem Finset.sum_of_empty {n m:ℤ} (h: n < m) (a: ℤ → ℝ) : ∑ i ∈ (Finset.Icc m n), a i = 0 := by
-  rw [Finset.sum_eq_zero]
+theorem sum_of_empty {n m:ℤ} (h: n < m) (a: ℤ → ℝ) : ∑ i ∈ Icc m n, a i = 0 := by
+  rw [sum_eq_zero]
   intro x hx
-  rw [Finset.mem_Icc] at hx
+  rw [mem_Icc] at hx
   linarith
 
 /-- Definition 7.1.1.  This is similar to Mathlib's `Finset.sum_Icc_succ_top` except that the latter involves summation over the natural numbers rather than integers. -/
-theorem Finset.sum_of_nonempty {n m:ℤ} (h: n ≥ m-1) (a: ℤ → ℝ) : ∑ i ∈ (Finset.Icc m (n+1)), a i = ∑ i ∈ (Finset.Icc m n), a i + a (n+1) := by
+theorem sum_of_nonempty {n m:ℤ} (h: n ≥ m-1) (a: ℤ → ℝ) : ∑ i ∈ Icc m (n+1), a i = ∑ i ∈ Icc m n, a i + a (n+1) := by
   rw [add_comm _ (a (n+1))]
-  convert Finset.sum_insert _
+  convert sum_insert _
   . ext i; simp; omega
   . infer_instance
   simp
 
+example (a: ℤ → ℝ) (m:ℤ) : ∑ i ∈ Icc m (m-2), a i = 0 := by sorry
+
+example (a: ℤ → ℝ) (m:ℤ) : ∑ i ∈ Icc m (m-1), a i = 0 := by sorry
+
+example (a: ℤ → ℝ) (m:ℤ) : ∑ i ∈ Icc m m, a i = a m := by sorry
+
+example (a: ℤ → ℝ) (m:ℤ) : ∑ i ∈ Icc m (m+1), a i = a m + a (m+1) := by sorry
+
+example (a: ℤ → ℝ) (m:ℤ) : ∑ i ∈ Icc m (m+2), a i = a m + a (m+1) + a (m+2) := by sorry
+
+/-- Remark 7.1.3 -/
+example (a: ℤ → ℝ) (m n:ℤ) : ∑ i ∈ Icc m n, a i = ∑ j ∈ Icc m n, a j := rfl
+
+/-- Lemma 7.1.4(a) / Exercise 7.1.1 -/
+theorem concat_finite_series {m n p:ℤ} (hmn: m ≤ n+1) (hpn : n ≤ p) (a: ℤ → ℝ) :
+  ∑ i ∈ Icc m n, a i + ∑ i ∈ Icc (n+1) p, a i = ∑ i ∈ Icc m p, a i := by sorry
+
+/-- Lemma 7.1.4(b) / Exercise 7.1.1 -/
+theorem shift_finite_series {m n k:ℤ} (a: ℤ → ℝ) :
+  ∑ i ∈ Icc m n, a i = ∑ i ∈ Icc (m+k) (n+k), a (i-k) := by sorry
+
+/-- Lemma 7.1.4(c) / Exercise 7.1.1 -/
+theorem finite_series_add {m n:ℤ} (a b: ℤ → ℝ) :
+  ∑ i ∈ Icc m n, (a i + b i) = ∑ i ∈ Icc m n, a i + ∑ i ∈ Icc m n, b i := by sorry
+
+/-- Lemma 7.1.4(d) / Exercise 7.1.1 -/
+theorem finite_series_const_mul {m n:ℤ}  (a: ℤ → ℝ) (c:ℝ) :
+  ∑ i ∈ Icc m n, c * a i = c * ∑ i ∈ Icc m n, a i := by sorry
+
+/-- Lemma 7.1.4(e) / Exercise 7.1.1 -/
+theorem abs_finite_series_le {m n:ℤ}   (a: ℤ → ℝ) (c:ℝ) :
+  |∑ i ∈ Icc m n, a i| ≤ ∑ i ∈ Icc m n, |a i| := by sorry
+
+/-- Lemma 7.1.4(f) / Exercise 7.1.1 -/
+theorem finite_series_of_le {m n:ℤ}  {a b: ℤ → ℝ} (h: ∀ i, m ≤ i → i ≤ n → a i ≤ b i) :
+  ∑ i ∈ Icc m n, a i ≤ ∑ i ∈ Icc m n, b i := by sorry
+
+-- This lemma is not part of the original textbook, but proven similarly to the other results above, and is handy.
+#check sum_congr
+
+/-- Proposition 7.1.8.  There is an unfortunate hack here in that one needs to extend the expressions `f g i` and `f h i` outside of `Icc 1 n`, leading to a certain uglification of the code. -/
+theorem finite_series_of_rearrange {n:ℕ} {Y:Type*} (X: Finset Y) (hcard: X.card = n) (f: X → ℝ) (g h: Icc (1:ℤ) n → X) (hg: Function.Bijective g) (hh: Function.Bijective h) : ∑ i ∈ Icc (1:ℤ) n, (if hi:i ∈ Icc (1:ℤ) n then f (g ⟨ i, hi ⟩) else 0) = ∑ i ∈ Icc (1:ℤ) n, (if hi: i ∈ Icc (1:ℤ) n then f (h ⟨ i, hi ⟩) else 0) := by
+  -- This proof is written to broadly follow the structure of the original text.
+  revert X n
+  intro n
+  induction' n with n hn
+  . simp [sum_of_empty (show 0 < 1 by norm_num) (fun _ ↦ 0)]
+  -- A technical step: we extend g, h to the entire integers using a slightly artificial map π
+  intro X hX f g h hg hh
+  set π : ℤ → Icc (1:ℤ) (n+1) := fun i ↦ if hi: i ∈ Icc (1:ℤ) (n+1) then ⟨ i, hi ⟩ else ⟨ 1, by simp ⟩
+  have hπ (g : Icc (1:ℤ) (n+1) → X) : ∑ i ∈ Icc (1:ℤ) (n+1), (if hi:i ∈ Icc (1:ℤ) (n+1) then f (g ⟨ i, hi ⟩) else 0) = ∑ i ∈ Icc (1:ℤ) (n+1), f (g (π i)) := by
+    apply sum_congr rfl _
+    intro i hi
+    simp only [hi, ↓reduceDIte, π]
+  simp only [Nat.cast_add, Nat.cast_one, Int.natCast_add, Int.cast_ofNat_Int, hπ]
+  rw [sum_of_nonempty (by linarith) _]
+  set x := g (π (n+1))
+
+  obtain ⟨ ⟨ j, hj' ⟩, hj ⟩ := Function.Bijective.surjective hh x
+  simp at hj'
+  obtain ⟨ hj1, hj2 ⟩ := hj'
+  set h' : ℤ → X := fun i ↦ if (i:ℤ) < j then h (π i) else h (π (i+1))
+  have :  ∑ i ∈ Icc (1:ℤ) (n + 1), f (h (π i)) = ∑ i ∈ Icc (1:ℤ) n, f (h' i) + f x := calc
+    _ = ∑ i ∈ Icc (1:ℤ) j, f (h (π i)) + ∑ i ∈ Icc (j+1:ℤ) (n + 1), f (h (π i)) := by
+      convert (concat_finite_series _ _ _).symm
+      all_goals linarith
+    _ = ∑ i ∈ Icc (1:ℤ) (j-1), f (h (π i)) + f ( h (π j) ) + ∑ i ∈ Icc (j+1:ℤ) (n + 1), f (h (π i)) := by
+      congr
+      convert sum_of_nonempty _ _ <;> simp
+      tauto
+    _ = ∑ i ∈ Icc (1:ℤ) (j-1), f (h (π i)) + f x + ∑ i ∈ Icc (j:ℤ) n, f (h (π (i+1))) := by
+      congr 1
+      . simp [←hj, π,hj1, hj2]
+      convert (shift_finite_series _).symm
+      simp
+    _ = ∑ i ∈ Icc (1:ℤ) (j-1), f (h (π i)) + ∑ i ∈ Icc (j:ℤ) n, f (h (π (i+1))) + f x := by abel
+    _ = ∑ i ∈ Icc (1:ℤ) (j-1), f (h' i) + ∑ i ∈ Icc (j:ℤ) n, f (h' i) + f x := by
+      congr 2
+      all_goals apply sum_congr rfl _; intro i hi; simp [h'] at hi ⊢
+      . have : i < j := by linarith
+        simp [this]
+      have : ¬ i < j := by linarith
+      simp [this]
+    _ = _ := by
+      congr
+      convert concat_finite_series _ _ _
+      all_goals linarith
+  rw [this]
+  congr 1
+  have g_ne_x {i:ℤ} (hi : i ∈ Icc (1:ℤ) n) : g (π i) ≠ x := by
+    simp at hi
+    have hi'' : i ≤ n+1 := by linarith
+    simp [x, (Function.Bijective.injective hg).eq_iff, π, hi.1, hi'']
+    linarith
+  have h'_ne_x {i:ℤ} (hi : i ∈ Icc (1:ℤ) n) : h' i ≠ x := by
+    simp at hi
+    have hi' : 0 ≤ i := by linarith
+    have hi'' : i ≤ n+1 := by linarith
+    by_cases hlt: i < j
+    all_goals by_contra! heq
+    all_goals simp [h', hlt, ←hj, (Function.Bijective.injective hh).eq_iff, ←Subtype.val_inj, π, hi.1, hi.2, hi',hi''] at heq
+    . linarith
+    contrapose! hlt; linarith
+  set gtil : Icc (1:ℤ) n → X.erase x := fun i ↦ ⟨ (g (π i)).val, by simp [mem_erase, Subtype.val_inj, g_ne_x] ⟩
+  set htil : Icc (1:ℤ) n → X.erase x := fun i ↦ ⟨ (h' i).val, by simp [mem_erase, Subtype.val_inj, h'_ne_x] ⟩
+  set ftil : X.erase x → ℝ := fun y ↦ f ⟨ y.val, by obtain ⟨ y, hy ⟩ := y; simp at hy ⊢; tauto ⟩
+  have why : Function.Bijective gtil := by sorry
+  have why2 : Function.Bijective htil := by sorry
+  calc
+    _ = ∑ i ∈ Icc (1:ℤ) n, if hi: i ∈ Icc (1:ℤ) n then ftil (gtil ⟨ i, hi ⟩ ) else 0 := by
+      apply sum_congr rfl _
+      intro i hi
+      simp [hi, gtil, ftil]
+    _ = ∑ i ∈ Icc (1:ℤ) n, if hi: i ∈ Icc (1:ℤ) n then ftil (htil ⟨ i, hi ⟩ ) else 0 := by
+      convert hn _ _ ftil gtil htil why why2
+      rw [Finset.card_erase_of_mem _, hX]
+      . simp
+      simp [x]
+    _ = _ := by
+      apply sum_congr rfl _
+      intro i hi
+      simp [hi, htil, ftil]
+
+
+
+end Finset
