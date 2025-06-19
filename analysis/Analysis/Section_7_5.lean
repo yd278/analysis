b2a88260b86e3e2d36004bbcacbce03790879da1
Terence Tao <tao@math.ucla.edu>
2025-06-19 13:17:51 -0700

    Finish 7.5.1(a)

--- analysis/Analysis/Section_7_5.lean
+++ analysis/Analysis/Section_7_5.lean
@@ -14,14 +14,69 @@ Main constructions and results of this section:
 
 namespace Chapter7
 
-/-- Theorem 7.5.1 (Root test) -/
+/-- Theorem 7.5.1 (Root test).  A technical condition `hbound` is needed to ensure the limsup is finite. -/
 theorem Series.root_test_pos {s : Series}
+  (hbound : Filter.IsBoundedUnder (· ≤ ·) Filter.atTop fun n ↦ |s.seq n| ^ (1 / (n:ℝ)) )
   (h : Filter.limsup (fun n ↦ |s.seq n|^(1/(n:ℝ))) Filter.atTop < 1) : s.absConverges := by
     -- This proof is written to follow the structure of the original text.
-    sorry
+    set α := Filter.limsup (fun n ↦ |s.seq n|^(1/(n:ℝ))) Filter.atTop
+    have hpos : 0 ≤ α := by
+      apply Filter.le_limsup_of_frequently_le _ hbound
+      apply Filter.Frequently.of_forall
+      intros; positivity
+    set ε := (1-α)/2
+    have hε : 0 < ε := by simp [ε]; linarith
+    have hε' : α < α+ε := by linarith
+    have hα : α + ε < 1 := by simp [ε]; linarith
+    have hα' : 0 < α + ε := by linarith
+    have := Filter.eventually_lt_of_limsup_lt hε' hbound
+    rw [Filter.eventually_atTop] at this
+    obtain ⟨ N', hN ⟩ := this
+    set N := max N' (max s.m 1)
+    have (n:ℤ) (hn: n ≥ N) : |s.seq n| ≤ (α + ε)^n := by
+      have : n ≥ N' := by omega
+      have npos : 0 < n := by omega
+      specialize hN n this
+      replace hN := le_of_lt hN
+      calc
+        _ = (|s.seq n|^(1/(n:ℝ)))^n := by
+          rw [←Real.rpow_intCast, ←Real.rpow_mul (by positivity)]
+          convert (Real.rpow_one _).symm
+          field_simp
+        _ ≤ _ := by
+          convert pow_le_pow_left₀ (by positivity) hN n.toNat
+          all_goals convert zpow_natCast _ _; omega
+    set k := (N - s.m).toNat
+    have hNk : N = s.m + k := by omega
+    have hgeom : (fun n ↦ (α+ε) ^ n : Series).converges := by
+      simp [converges_geom_iff, abs_of_pos hα', hα]
+    replace hgeom := (converges_from _ N.toNat).mp hgeom
+    have : (s.from N).absConverges := by
+      apply (converges_of_le _ _ hgeom).1
+      . simp; omega
+      intro n hn
+      simp [Series.from] at hn
+      have hn' : n ≥ 0 := by omega
+      simp [hn.1, hn.2, hn']
+      convert this n hn.2
+      convert (zpow_natCast _ _).symm
+      omega
+    unfold absConverges at this ⊢
+    rw [converges_from _ k]
+    convert this
+    simp
+    constructor
+    . omega
+    ext n
+    by_cases hnm : n ≥ s.m
+    all_goals simp [hnm]
+    by_cases hn: n ≥ N
+    all_goals simp [hn]; intros; omega
+
 
 /-- Theorem 7.5.1 (Root test) -/
 theorem Series.root_test_neg {s : Series}
+  (hbound : Filter.IsBoundedUnder (· ≤ ·) Filter.atTop fun n ↦ |s.seq n| ^ (1 / (n:ℝ)) )
   (h : Filter.limsup (fun n ↦ |s.seq n|^(1/(n:ℝ))) Filter.atTop > 1) : s.diverges := by
     -- This proof is written to follow the structure of the original text.
     sorry
