da72c4148e7086c476c2c87a53984343c49c4efd
Rado Kirov <rkirov@gmail.com>
2025-07-07 22:17:10 -0700

    Add two theorems to ch3.4
    The image of any set is a subset of the codomain, and the preimage is a
    subset of the domain.

--- analysis/Analysis/Section_3_4.lean
+++ analysis/Analysis/Section_3_4.lean
@@ -49,6 +49,15 @@ theorem SetTheory.Set.image_eq_specify {X Y:Set} (f:X → Y) (S: Set) :
 theorem SetTheory.Set.image_eq_image {X Y:Set} (f:X → Y) (S: Set):
     (image f S: _root_.Set Object) = Subtype.val '' (f '' {x | x.val ∈ S}) := by sorry
 
+theorem SetTheory.Set.image_in_codomain {X Y:Set} (f:X → Y) (S: Set) :
+    image f S ⊆ Y := by
+  rw [SetTheory.Set.subset_def]
+  intro x h
+  rw [mem_image] at h
+  obtain ⟨ x', hx', hf ⟩ := h
+  rw [← hf]
+  exact (f x').property
+
 /-- Example 3.4.2 -/
 abbrev f_3_4_2 : nat → nat := fun n ↦ (2*n:ℕ)
 
@@ -77,6 +86,13 @@ theorem SetTheory.Set.mem_preimage {X Y:Set} (f:X → Y) (U: Set) (x:X) :
 /-- Connection with Mathlib's notion of preimage. -/
 theorem SetTheory.Set.preimage_eq {X Y:Set} (f:X → Y) (U: Set) :
     ((preimage f U): _root_.Set Object) = Subtype.val '' (f⁻¹' {y | y.val ∈ U}) := by sorry
+
+theorem SetTheory.Set.preimage_in_domain {X Y:Set} (f:X → Y) (U: Set) :
+    (preimage f U) ⊆ X := by
+  rw [subset_def]
+  intro x h
+  rw [preimage] at h
+  exact specification_axiom h
 
 /-- Example 3.4.5 -/
 theorem SetTheory.Set.preimage_f_3_4_2 : preimage f_3_4_2 {2,4,6} = {1,2,3} := by sorry
