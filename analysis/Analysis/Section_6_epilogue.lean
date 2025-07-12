8ebc706035ebcdcf8ec0254fbb32546c737a1ca3
Li Xuanji <xuanji@duolingo.com>
2025-07-12 00:29:02 -0400

    fix

--- analysis/Analysis/Section_6_epilogue.lean
+++ analysis/Analysis/Section_6_epilogue.lean
@@ -145,12 +145,12 @@ theorem Chapter6.Sequence.inf_eq_sInf (a:ℕ → ℝ):
     (a:Sequence).inf = sInf (Set.range (fun n ↦ (a n:EReal))) := by sorry
 
 theorem Chapter6.Sequence.bddAbove_iff (a:ℕ → ℝ):
-    (a:Sequence).BddAbove ↔ BddAbove (Set.range a) := by sorry
+    (a:Sequence).BddAbove ↔ _root_.BddAbove (Set.range a) := by sorry
 
 theorem Chapter6.Sequence.bddBelow_iff (a:ℕ → ℝ):
-    (a:Sequence).BddBelow ↔ BddBelow (Set.range a) := by sorry
+    (a:Sequence).BddBelow ↔ _root_.BddBelow (Set.range a) := by sorry
 
-theorem Chapter6.Sequence.Monotone_iff (a:ℕ → ℝ): (a:Sequence).isMonotone ↔ Monotone a := by sorry
+theorem Chapter6.Sequence.Monotone_iff (a:ℕ → ℝ): (a:Sequence).IsMonotone ↔ Monotone a := by sorry
 
 theorem Chapter6.Sequence.Antitone_iff (a:ℕ → ℝ): (a:Sequence).IsAntitone ↔ Antitone a := by sorry
 
