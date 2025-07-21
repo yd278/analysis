27bb862be7cdec8cc2f8350b9950655c4cee27d0
Terence Tao <tao@math.ucla.edu>
2025-07-20 22:42:22 -0700

    9.7

--- analysis/Analysis/Section_9_7.lean
+++ analysis/Analysis/Section_9_7.lean
@@ -4,7 +4,7 @@ import Analysis.Section_9_4
 
 
 /-!
-# Analysis I, Section 9.7
+# Analysis I, Section 9.7: The intermediate value theorem
 
 I have attempted to make the translation as faithful a paraphrasing as possible of the original
 text.  When there is a choice between a more idiomatic Lean solution and a more faithful
@@ -13,7 +13,7 @@ the Lean code could be "golfed" to be more elegant and idiomatic, but I have con
 doing so.
 
 Main constructions and results of this section:
-- The intermediate value theorem
+- The intermediate value theorem.
 -/
 
 namespace Chapter9
@@ -31,13 +31,10 @@ theorem intermediate_value {a b:ℝ} (hab: a < b) {f:ℝ → ℝ} (hf: Continuou
     replace hya : f a < y := by contrapose! hya; linarith
     replace hyb : y < f b := by contrapose! hyb; linarith
     set E := {x | x ∈ Set.Icc a b ∧ f x < y}
-    have hE : E ⊆ Set.Icc a b := by
-      rintro x ⟨hx₁, hx₂⟩; exact hx₁
+    have hE : E ⊆ Set.Icc a b := by intro x ⟨hx₁, hx₂⟩; exact hx₁
     have hE_bdd : BddAbove E := BddAbove.mono hE bddAbove_Icc
-    have hEa : a ∈ E := by
-      simp [E, hya, le_of_lt hab]
-    have hE_nonempty : E.Nonempty := by
-      use a
+    have hEa : a ∈ E := by simp [E, hya, le_of_lt hab]
+    have hE_nonempty : E.Nonempty := by use a
     set c := sSup E
     have hc : c ∈ Set.Icc a b := by
       simp
@@ -50,23 +47,19 @@ theorem intermediate_value {a b:ℝ} (hab: a < b) {f:ℝ → ℝ} (hf: Continuou
       have hxe (n:ℕ) : ∃ x ∈ E, c - 1/(n+1:ℝ) < x := by
         have : 1/(n+1:ℝ) > 0 := by positivity
         replace : c - 1/(n+1:ℝ) < sSup E := by linarith
-        exact exists_lt_of_lt_csSup hE_nonempty this
+        solve_by_elim [exists_lt_of_lt_csSup]
       set x := fun n ↦ (hxe n).choose
       have hx1 (n:ℕ) : x n ∈ E := (hxe n).choose_spec.1
       have hx2 (n:ℕ) : c - 1/(n+1:ℝ) < x n := (hxe n).choose_spec.2
       have : Filter.Tendsto x Filter.atTop (nhds c) := by
         apply Filter.Tendsto.squeeze (g := fun j ↦ c - 1/(j+1:ℝ)) (h := fun j ↦ c) (f := x)
-        . convert Filter.Tendsto.const_sub c (c:=0) _
-          . simp
-          exact tendsto_one_div_add_atTop_nhds_zero_nat
+        . convert Filter.Tendsto.const_sub c tendsto_one_div_add_atTop_nhds_zero_nat
+          simp
         . exact tendsto_const_nhds
         . exact fun n ↦ le_of_lt (hx2 n)
         exact fun n ↦ ConditionallyCompleteLattice.le_csSup _ _ hE_bdd (hx1 n)
       replace := Filter.Tendsto.comp_of_continuous hc (hf.continuousWithinAt hc) (fun n ↦ hE (hx1 n)) this
-      have hfxny (n:ℕ) : f (x n) ≤ y := by
-        specialize hx1 n
-        simp [E] at hx1
-        exact le_of_lt hx1.2
+      have hfxny (n:ℕ) : f (x n) ≤ y := by specialize hx1 n; simp [E] at hx1; exact le_of_lt hx1.2
       exact le_of_tendsto' this hfxny
     have hne : c ≠ b := by
       contrapose! hfc_upper
@@ -75,8 +68,7 @@ theorem intermediate_value {a b:ℝ} (hab: a < b) {f:ℝ → ℝ} (hf: Continuou
     have hfc_lower : y ≤ f c := by
       have : ∃ N:ℕ, ∀ n ≥ N, (c+1/(n+1:ℝ)) < b := by
         obtain ⟨ N, hN ⟩ := exists_nat_gt (1/(b-c))
-        use N
-        intro n hn
+        use N; intro n hn
         have hpos : 0 < b-c := by linarith
         have : 1/(n+1:ℝ) < b-c := by
           rw [one_div_lt (by positivity) (by positivity)]
@@ -88,34 +80,28 @@ theorem intermediate_value {a b:ℝ} (hab: a < b) {f:ℝ → ℝ} (hf: Continuou
         simp only [Set.mem_Icc, le_of_lt (hN n hn), and_true]
         have : 1/(n+1:ℝ) > 0 := by positivity
         replace : c + 1/(n+1:ℝ) > c := by linarith
-        simp at hc
-        linarith
+        simp at hc; linarith
       have : ∀ n ≥ N, c + 1/(n+1:ℝ) ∉ E := by
         intro n _
         have : 1/(n+1:ℝ) > 0 := by positivity
         replace : c + 1/(n+1:ℝ) > c := by linarith
-        exact notMem_of_csSup_lt this hE_bdd
+        solve_by_elim [notMem_of_csSup_lt]
       replace : ∀ n ≥ N, f (c + 1/(n+1:ℝ)) ≥ y := by
-        intro n hn
-        specialize this n hn
-        contrapose! this
+        intro n hn; specialize this n hn; contrapose! this
         simp only [Set.mem_Icc, Set.mem_setOf_eq, E, this, le_of_lt (hN n hn), and_true]
         have := hmem n hn
         simp only [Set.mem_Icc] at this
         tauto
       have hconv : Filter.Tendsto (fun n:ℕ ↦ c + 1/(n+1:ℝ)) Filter.atTop (nhds c) := by
-        convert Filter.Tendsto.const_add c (c:=0) _
-        . simp
-        exact tendsto_one_div_add_atTop_nhds_zero_nat
+        convert Filter.Tendsto.const_add c tendsto_one_div_add_atTop_nhds_zero_nat
+        simp
       replace hf := (hf.continuousWithinAt hc).tendsto
       rw [nhdsWithin.eq_1] at hf
       have hconv' : Filter.Tendsto (fun n:ℕ ↦ c + 1/(n+1:ℝ)) Filter.atTop (Filter.principal (Set.Icc a b)) := by
-        simp only [Filter.tendsto_principal, Filter.eventually_atTop]
-        use N
+        simp only [Filter.tendsto_principal, Filter.eventually_atTop]; use N
       replace hconv' := Filter.tendsto_inf.mpr ⟨ hconv, hconv' ⟩
       apply ge_of_tendsto (Filter.Tendsto.comp hf hconv') _
-      simp only [Function.comp_apply, Filter.eventually_atTop]
-      use N
+      simp only [Function.comp_apply, Filter.eventually_atTop]; use N
     linarith
   sorry
 
