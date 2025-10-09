fbe191b2c2d389ac67b34944d39d49f9e131712d
Y. Ding <argentumdyz@gmail.com>
2025-10-09 16:20:03 -0700

    exercise 2.2.1

--- analysis/Analysis/Section_2_2.lean
+++ analysis/Analysis/Section_2_2.lean
@@ -98,7 +98,10 @@ theorem Nat.add_comm (n m:Nat) : n + m = m + n := by
 /-- Proposition 2.2.5 (Addition is associative) / Exercise 2.2.1
     Compare with Mathlib's `Nat.add_assoc`. -/
 theorem Nat.add_assoc (a b c:Nat) : (a + b) + c = a + (b + c) := by
-  sorry
+  revert a; apply induction 
+  . rw[zero_add, zero_add]
+  intro n ih
+  rw[succ_add, succ_add, succ_add, ih]
 
 /-- Proposition 2.2.6 (Cancellation law).
     Compare with Mathlib's `Nat.add_left_cancel`. -/
