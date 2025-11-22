aeb35dc7b1ce7178e5f33df25b6372af517dc9f5
Terence Tao <tao@math.ucla.edu>
2025-11-21 19:47:12 -0800

    more Sec 1.3.4

--- analysis/Analysis/MeasureTheory/Section_1_3_4.lean
+++ analysis/Analysis/MeasureTheory/Section_1_3_4.lean
@@ -58,4 +58,78 @@ def RealSimpleFunction.AbsolutelyIntegrable.integ_eq {d:ℕ} {f: EuclideanSpace'
 
 def ComplexSimpleFunction.AbsolutelyIntegrable.integ_eq {d:ℕ} {f: EuclideanSpace' d → ℂ} (hf: ComplexSimpleFunction f) (hfi: hf.AbsolutelyIntegrable) : hf.integ = (hf.absolutelyIntegrable_iff'.mp hfi).integ := by sorry
 
+theorem RealAbsolutelyIntegrable.add {d:ℕ} {f g: EuclideanSpace' d → ℝ} (hf: RealAbsolutelyIntegrable f) (hg: RealAbsolutelyIntegrable g) : RealAbsolutelyIntegrable (f + g) := by sorry
+
+theorem ComplexAbsolutelyIntegrable.add {d:ℕ} {f g: EuclideanSpace' d → ℂ} (hf: ComplexAbsolutelyIntegrable f) (hg: ComplexAbsolutelyIntegrable g) : ComplexAbsolutelyIntegrable (f + g) := by sorry
+
+theorem RealAbsolutelyIntegrable.sub {d:ℕ} {f g: EuclideanSpace' d → ℝ} (hf: RealAbsolutelyIntegrable f) (hg: RealAbsolutelyIntegrable g) : RealAbsolutelyIntegrable (f - g) := by sorry
+
+theorem ComplexAbsolutelyIntegrable.sub {d:ℕ} {f g: EuclideanSpace' d → ℂ} (hf: ComplexAbsolutelyIntegrable f) (hg: ComplexAbsolutelyIntegrable g) : ComplexAbsolutelyIntegrable (f - g) := by sorry
+
+theorem RealAbsolutelyIntegrable.smul {d:ℕ} {f: EuclideanSpace' d → ℝ} (hf: RealAbsolutelyIntegrable f) (c:ℝ) : RealAbsolutelyIntegrable (c • f) := by sorry
+
+theorem ComplexAbsolutelyIntegrable.smul {d:ℕ} {f: EuclideanSpace' d → ℂ} (hf: ComplexAbsolutelyIntegrable f) (c:ℂ) : ComplexAbsolutelyIntegrable (c • f) := by sorry
+
+theorem RealAbsolutelyIntegrable.of_neg {d:ℕ} {f: EuclideanSpace' d → ℝ} (hf: RealAbsolutelyIntegrable f) : RealAbsolutelyIntegrable (-f) := by sorry
+
+theorem ComplexAbsolutelyIntegrable.of_neg {d:ℕ} {f: EuclideanSpace' d → ℂ} (hf: ComplexAbsolutelyIntegrable f) : ComplexAbsolutelyIntegrable (-f) := by sorry
+
+@[ext]
+structure PreL1 (d:ℕ) where
+  f : EuclideanSpace' d → ℂ
+  integrable : ComplexAbsolutelyIntegrable f
+
+def ComplexAbsolutelyIntegrable.to_PreL1 {d:ℕ} {f: EuclideanSpace' d → ℂ} (hf: ComplexAbsolutelyIntegrable f) : PreL1 d := ⟨ f, hf ⟩
+
+instance PreL1.inst_AddZeroClass {d:ℕ} : AddZeroClass (PreL1 d) := {
+  zero := ⟨ 0, by sorry ⟩
+  add F G := ⟨ F.f + G.f, F.integrable.add G.integrable ⟩
+  zero_add := by sorry
+  add_zero := by sorry
+}
+
+instance PreL1.inst_addCommMonoid {d:ℕ} : AddCommMonoid (PreL1 d) := {
+  add_assoc := by sorry
+  add_comm := by sorry
+  nsmul := nsmulRec
+}
+
+instance PreL1.inst_Neg {d:ℕ} : Neg (PreL1 d) := {
+  neg F := ⟨ -F.f, F.integrable.of_neg ⟩
+}
+
+instance PreL1.inst_module {d:ℕ} : Module ℂ (PreL1 d) := {
+  smul c F := ⟨ c • F.f, F.integrable.smul c ⟩
+  zero_smul := by sorry
+  smul_zero := by sorry
+  one_smul := by sorry
+  mul_smul := by sorry
+  smul_add := by sorry
+  add_smul := by sorry
+}
+
+noncomputable instance PreL1.inst_seminormedAddCommGroup {d:ℕ} : SeminormedAddCommGroup (PreL1 d) := {
+  norm F := F.integrable.norm
+  neg F := ⟨ -F.f, F.integrable.of_neg ⟩
+  zsmul := zsmulRec
+  neg_add_cancel := by sorry
+  dist_self := by sorry
+  dist_comm := by sorry
+  dist_triangle := by sorry
+}
+
+instance PreL1.inst_normedSpace {d:ℕ} : NormedSpace ℂ (PreL1 d) := {
+  norm_smul_le := by sorry
+}
+
+theorem PreL1.dist_eq {d:ℕ} (F G: PreL1 d) : dist F G = ‖F-G‖ := rfl
+
+noncomputable abbrev L1 (d:ℕ) := SeparationQuotient (PreL1 d)
+
+def ComplexAbsolutelyIntegrable.toL1 {d:ℕ} {f:EuclideanSpace' d → ℂ} (hf: ComplexAbsolutelyIntegrable f) : L1 d := SeparationQuotient.mk hf.to_PreL1
+
+theorem L1.dist_eq {d:ℕ} (f g: EuclideanSpace' d → ℂ) (hf: ComplexAbsolutelyIntegrable f) (hg: ComplexAbsolutelyIntegrable g) : dist hf.toL1 hg.toL1 = (hf.sub hg).norm := by sorry
+
+theorem L1.dist_eq_zero {d:ℕ} (f g: EuclideanSpace' d → ℂ) (hf: ComplexAbsolutelyIntegrable f) (hg: ComplexAbsolutelyIntegrable g) : dist hf.toL1 hg.toL1 = 0 ↔ AlmostEverywhereEqual f g := by sorry
+
 
