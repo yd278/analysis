3d98acf0a56b69b563de232d36a7a95be1089c99
Yuzheng.Ding <yuzheng.q.ding@gmail.com>
2025-11-25 14:13:30 -0800

    Squash merge 5_1 into main

--- analysis/Analysis/Section_5_1.lean
+++ analysis/Analysis/Section_5_1.lean
@@ -171,11 +171,54 @@ example : (0.1:ℚ).Steady ((fun n:ℕ ↦ (10:ℚ) ^ (-(n:ℤ)-1) ):Sequence) :
 /--
 Example 5.1.5: The sequence 0.1, 0.01, 0.001, ... is not 0.01-steady. Left as an exercise.
 -/
-example : ¬(0.01:ℚ).Steady ((fun n:ℕ ↦ (10:ℚ) ^ (-(n:ℤ)-1) ):Sequence) := by sorry
+example : ¬(0.01:ℚ).Steady ((fun n:ℕ ↦ (10:ℚ) ^ (-(n:ℤ)-1) ):Sequence) := by
+  rw[Rat.Steady.coe]
+  by_contra h
+  specialize h 0 1; simp[Rat.Close] at h
+  norm_num at h
+  simp[abs_of_pos] at h
+  grind
 
 /-- Example 5.1.5: The sequence 1, 2, 4, 8, ... is not ε-steady for any ε. Left as an exercise.
 -/
-example (ε:ℚ) : ¬ ε.Steady ((fun n:ℕ ↦ (2 ^ (n+1):ℚ) ):Sequence) := by sorry
+lemma two_pow_gt (n : ℕ) : n + 1 <  2 ^ (n + 2)  := by
+  induction' n with k hind
+  . simp
+  rw[add_assoc k 1 2,add_comm 1 2,← add_assoc,pow_succ,mul_two]
+  linarith
+
+
+example (ε:ℚ) : ¬ ε.Steady ((fun n:ℕ ↦ (2 ^ (n):ℚ) ):Sequence) := by
+  simp;use 0;simp[Rat.Close]
+  set c := ⌈ε⌉  
+  set p := (|c| + 2)
+  have : p >= 0 := by
+    simp[p]
+    calc
+      _ <= |c| := by simp
+      _ <= _ := by simp
+  use p;
+  simp[this]
+  rw[abs_of_nonpos]
+  pick_goal 2; rw[sub_nonpos]
+  induction' p.toNat with k hind
+  . simp
+  ring_nf;linarith
+  rw[neg_sub]
+  have : p.toNat  = |c|.toNat + 2 := by
+    simp[p]
+    apply Int.toNat_add_nat
+    simp
+  simp[this]
+  suffices h : |c|.toNat  < (2:ℚ) ^ (|c|.toNat  + 2) - 1 from by
+    calc
+      _ <= (c:ℚ)  := by exact Int.le_ceil ε
+      _ <= |c| := by norm_cast; exact le_abs_self c
+      _ <= |c|.toNat := by norm_cast; exact Int.self_le_toNat |c|
+      _ < _ := by assumption
+  have := two_pow_gt (|c|.toNat)
+  apply lt_sub_left_of_add_lt
+  rw[add_comm];norm_cast
 
 /-- Example 5.1.5:The sequence 2, 2, 2, ... is ε-steady for any ε > 0.
 -/
@@ -254,7 +297,16 @@ Example 5.1.7
 The sequence 10, 0, 0, ... is eventually ε-steady for every ε > 0. Left as an exercise.
 -/
 lemma Sequence.ex_5_1_7_d {ε:ℚ} (hε:ε>0) :
-    ε.EventuallySteady ((fun n:ℕ ↦ if n=0 then (10:ℚ) else (0:ℚ) ):Sequence) := by sorry
+    ε.EventuallySteady ((fun n:ℕ ↦ if n=0 then (10:ℚ) else (0:ℚ) ):Sequence) := by
+      use 1; simp
+      intro m hm n hn;
+      simp[Rat.Close]
+      simp_all
+      have: ¬m<=0:=by grind
+      simp[this]
+      have: ¬n<=0:=by grind
+      simp[this]
+      grind
 
 abbrev Sequence.IsCauchy (a:Sequence) : Prop := ∀ ε > (0:ℚ), ε.EventuallySteady a
 
@@ -299,14 +351,101 @@ noncomputable def Sequence.sqrt_two : Sequence := (fun n:ℕ ↦ ((⌊ (Real.sqr
 /--
   Example 5.1.10. (This requires extensive familiarity with Mathlib's API for the real numbers.)
 -/
-theorem Sequence.ex_5_1_10_a : (1:ℚ).Steady sqrt_two := by sorry
+
+lemma partial_fract (x n m b: ℝ) (hb: b > 0) (hn: n >= b⁻¹ ) (hm : m >= b⁻¹ )  :  |(⌊x * n⌋ / n) - (⌊x * m⌋ / m)| ≤ b := by
+  wlog hnm : n >= m
+  . specialize this x m n b hb hm hn (by grind)
+    rwa[abs_sub_comm]
+  have : b⁻¹ > 0 := by exact Right.inv_pos.mpr hb
+  rw[div_sub_div _ _ (by grind ) (by grind)]
+  have hnf := Int.self_sub_fract (x * n) 
+  have hmf := Int.self_sub_fract (x * m) 
+  have hhnf := Int.fract_lt_one (x * n)
+  have hhmf := Int.fract_lt_one (x * m)
+  have hnfp := Int.fract_nonneg (x * n)
+  have hmfp := Int.fract_nonneg (x * m)
+  rw[← hnf, ← hmf]
+  set p := Int.fract (x * n)
+  set q := Int.fract (x * m)
+  have: ((x * ↑n - p) * ↑m - ↑n * (x * ↑m - q)) / (↑n * ↑m) = (q / m - p / n) := by grind
+  simp[this]
+  rw[abs_le]
+  split_ands
+  .
+    apply neg_le_of_neg_le
+    rw[neg_sub]
+    have hp : q / m >= 0 := by
+      apply div_nonneg hmfp (by grind)
+    have hq : p / n < b := by
+      rw[div_lt_iff₀' (by grind)]
+      calc
+        _ < (1:ℝ) := hhnf
+        _ = b⁻¹ * b := by rw[inv_mul_cancel₀ (by grind)] 
+        _ ≤  _ := by exact (mul_le_mul_iff_of_pos_right hb).mpr hn
+    grind
+  . 
+    have hp : p / n >= 0 := by
+      apply div_nonneg hnfp (by grind)
+    have hq : q / m < b := by
+      rw[div_lt_iff₀' (by grind)]
+      calc
+        _ < (1:ℝ) := hhmf
+        _ = b⁻¹ * b := by rw[inv_mul_cancel₀ (by grind)] 
+        _ ≤  _ := by exact (mul_le_mul_iff_of_pos_right hb).mpr hm
+    grind
+
+theorem Sequence.ex_5_1_10_a : (1:ℚ).Steady sqrt_two := by
+  intro n hn m hm
+  simp_all[sqrt_two,Rat.Close]
+  wlog hnm : n >= m
+  . 
+    specialize this m n hm hn (by grind)
+    rwa[abs_sub_comm]
+  lift n to ℕ using hn
+  lift m to ℕ using hm
+  simp_all
+  rify
+  have := partial_fract (Real.sqrt 2) (10^n) (10^m) 1  (by simp) (by simp; norm_cast; exact Nat.one_le_pow' n 9) (by simp; norm_cast; exact Nat.one_le_pow' m 9)
+  assumption
 
 /--
   Example 5.1.10. (This requires extensive familiarity with Mathlib's API for the real numbers.)
 -/
-theorem Sequence.ex_5_1_10_b : (0.1:ℚ).Steady (sqrt_two.from 1) := by sorry
 
-theorem Sequence.ex_5_1_10_c : (0.1:ℚ).EventuallySteady sqrt_two := by sorry
+theorem Sequence.ex_5_1_10_b : (0.1:ℚ).Steady (sqrt_two.from 1) := by 
+  intro n hn m hm
+  simp [sqrt_two] at hn hm
+  unfold Rat.Close
+  rw[(sqrt_two).from_eval hn,(sqrt_two).from_eval hm]
+  simp[sqrt_two, show 0 <= n by grind,show 0 <= m by grind]
+  lift n to ℕ using (by grind)
+  lift m to ℕ using (by grind)
+  simp at hn hm ⊢ 
+  have hlb (n:ℕ) (hn : n>= 1) : (10:ℝ) ^ n >= (0.1⁻¹) := by
+    set n' := n - 1
+    have : n = n' + 1 := by
+      simp[n']
+      exact (Nat.sub_eq_iff_eq_add hn).mp rfl
+    simp[this]
+    induction' n' with k hind
+    . 
+      simp
+      calc 
+        _ = (1 / 0.1:ℝ) := by exact inv_eq_one_div 0.1
+        _ = (1 / (1 / 10)) := by norm_num
+        _ <= _ := by simp
+    rw[pow_succ]
+    calc
+      _ <= (10:ℝ) ^ (k+1) := by exact hind
+      _ <= _ := by simp
+
+  have := partial_fract (Real.sqrt 2) (10 ^ n) (10 ^ m) 0.1 (by norm_num) (hlb n hn) (hlb m hm)
+  rify
+  assumption
+
+theorem Sequence.ex_5_1_10_c : (0.1:ℚ).EventuallySteady sqrt_two := by
+  use 1; simp[sqrt_two]
+  exact ex_5_1_10_b
 
 
 /-- Proposition 5.1.11. The harmonic sequence, defined as a₁ = 1, a₂ = 1/2, ... is a Cauchy sequence. -/
@@ -365,7 +504,26 @@ lemma Sequence.isBounded_def (a:Sequence) : a.IsBounded ↔ ∃ M ≥ 0, a.Bound
 example : BoundedBy ![1,-2,3,-4] 4 := by intro i; fin_cases i <;> norm_num
 
 /-- Example 5.1.13 -/
-example : ¬((fun n:ℕ ↦ (-1)^n * (n+1:ℚ)):Sequence).IsBounded := by sorry
+example : ¬((fun n:ℕ ↦ (-1)^n * (n+1:ℚ)):Sequence).IsBounded := by
+  by_contra h
+  obtain ⟨M, hM,hbon⟩ := h 
+  set m := ⌈M⌉
+  have hm : m >= 0 := by
+    qify
+    calc
+      _ <= M := hM
+      _ <= m := by exact Int.le_ceil M
+  specialize hbon m
+  simp[hm] at hbon
+  rw[abs_mul] at hbon
+  simp at hbon
+  rw[abs_of_pos] at hbon
+  have : M <= m.toNat := by
+    calc
+      _ <= (m:ℚ) := by exact Int.le_ceil M
+      _ = (m.toNat) := by norm_cast; exact Int.eq_natCast_toNat.mpr hm
+  grind
+  exact Nat.cast_add_one_pos m.toNat
 
 /-- Example 5.1.13 -/
 example : ((fun n:ℕ ↦ (-1:ℚ)^n):Sequence).IsBounded := by
@@ -398,18 +556,128 @@ lemma IsBounded.finite {n:ℕ} (a: Fin n → ℚ) : ∃ M ≥ 0,  BoundedBy a M 
   . grind
   convert h2; simp
 
+lemma IsBounded.steady {a: Sequence} {ε :ℚ }(ha: ε.Steady a) (hε : ε >= 0) : ∃M ≥ 0 , a.BoundedBy M := by
+  specialize ha a.n₀ (by simp)
+  set a₀ := |a a.n₀|
+  use a₀ + ε 
+  split_ands
+  . have : a₀ ≥ 0 := by simp[a₀] 
+    grind
+  intro m
+  specialize ha m
+  by_cases hm : m >= a.n₀
+  . specialize ha hm
+    set aₘ := |a.seq m|
+    unfold Rat.Close  at ha
+    apply le_add_of_sub_left_le
+    rw[abs_sub_comm] at ha
+    have : aₘ - a₀ <= |a m - a a.n₀| := by
+      have := abs_add (a m - a a.n₀) (a a.n₀)
+      have : aₘ <= |a m - a a.n₀| + a₀ := by
+        simp[aₘ,a₀]
+        simpa[sub_add_cancel] using this
+      exact abs_sub_abs_le_abs_sub (a.seq m) (a.seq a.n₀)
+    grind
+  push_neg at hm
+  have := a.vanish m hm
+  simp[this]
+  calc
+    _ <= a₀ := by simp[a₀]
+    _ <= _ := by grind
+
 /-- Lemma 5.1.15 (Cauchy sequences are bounded) / Exercise 5.1.1 -/
 lemma Sequence.isBounded_of_isCauchy {a:Sequence} (h: a.IsCauchy) : a.IsBounded := by
-  sorry
+  specialize h 1 (by simp)
+  choose N hN hste using h
+  rw[ge_iff_le,le_iff_exists_nonneg_add] at hN
+  choose c hc hdiff using hN
+  lift c to ℕ using hc
+  have hbon1 := IsBounded.steady (hste) (by simp)
+  obtain ⟨u1, hu1, hbon1⟩ := hbon1
+  set a' : Fin c → ℚ := fun i ↦ a (a.n₀ +i)
+  choose u2 hu2 hbon2 using IsBounded.finite a'
+  use max u1 u2
+  split_ands
+  . simp;tauto
+  intro i
+  by_cases hi : i >= N
+  . specialize hbon1 i
+    rw[from_eval _ hi] at hbon1
+    simp;left;assumption
+  push_neg at hi
+  by_cases hi' : i >= a.n₀
+  . 
+    rw[ge_iff_le,le_iff_exists_nonneg_add] at hi'
+    choose c' hc hca using hi'
+    lift c' to ℕ using hc
+    have hcc': c' < c := by
+      simpa[← hca,← hdiff] using hi
+    have : a i = a' ⟨c',hcc'⟩  := by
+      simp[a',hca]
+    simp[this];right
+    specialize hbon2 ⟨c',hcc'⟩ 
+    assumption
+  push_neg at hi'
+  have := a.vanish i hi'
+  simp[this];tauto
 
 /-- Exercise 5.1.2 -/
 theorem Sequence.isBounded_add {a b:ℕ → ℚ} (ha: (a:Sequence).IsBounded) (hb: (b:Sequence).IsBounded):
-    (a + b:Sequence).IsBounded := by sorry
+    (a + b:Sequence).IsBounded := by
+      choose ua hua hbona using ha
+      choose ub hub hbonb using hb
+      use ua+ub
+      refine ⟨by grind, ?_ ⟩ 
+      intro i
+      specialize hbona i
+      specialize hbonb i
+      by_cases hi : 0 <= i
+      . 
+        lift i to ℕ using hi
+        simp at hbona hbonb ⊢ 
+        calc
+          _ <= |a i| + |b i| := abs_add _ _
+          _ <= _ := by grind
+      simp[hi]
+      grind
 
 theorem Sequence.isBounded_sub {a b:ℕ → ℚ} (ha: (a:Sequence).IsBounded) (hb: (b:Sequence).IsBounded):
-    (a - b:Sequence).IsBounded := by sorry
+    (a - b:Sequence).IsBounded := by
+      choose ua hua hbona using ha
+      choose ub hub hbonb using hb
+      use ua+ub
+      refine ⟨by grind, ?_ ⟩ 
+      intro i
+      specialize hbona i
+      specialize hbonb i
+      by_cases hi : 0 <= i
+      . 
+        lift i to ℕ using hi
+        simp at hbona hbonb ⊢ 
+        rw[sub_eq_add_neg]
+        calc
+         _ <= |a i| + |-b i| := abs_add _ _
+         _ = |a i| + |b i| := by simp
+         _ <= _ := by grind
+      simp[hi]
+      grind
 
 theorem Sequence.isBounded_mul {a b:ℕ → ℚ} (ha: (a:Sequence).IsBounded) (hb: (b:Sequence).IsBounded):
-    (a * b:Sequence).IsBounded := by sorry
-
+    (a * b:Sequence).IsBounded := by
+      choose ua hua hbona using ha
+      choose ub hub hbonb using hb
+      use ua*ub
+      split_ands
+      . simp[mul_nonneg_iff];left;tauto
+      intro i
+      specialize hbona i
+      specialize hbonb i
+      by_cases hi : 0 <= i
+      . 
+        lift i to ℕ using hi
+        simp at hbona hbonb ⊢ 
+        rw[abs_mul];
+        apply mul_le_mul_of_nonneg
+        map_tacs[assumption;assumption;simp;assumption]
+      simp[hi,mul_nonneg_iff];left;tauto
 end Chapter5
