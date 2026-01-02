fb4916ab34a2894f825dbffceee74d72b2310732
Terence Tao <tao@math.ucla.edu>
2026-01-01 22:02:21 -0800

    Start Sec 1.4.2

--- analysis/Analysis/MeasureTheory/Section_1_4_2.lean
+++ analysis/Analysis/MeasureTheory/Section_1_4_2.lean
@@ -5,3 +5,45 @@ import Analysis.MeasureTheory.Section_1_4_1
 A companion to (the introduction to) Section 1.4.2 of the book "An introduction to Measure Theory".
 
 -/
+
+/-- Definition 1.4.12 (Sigma algebra) -/
+class ConcreteSigmaAlgebra (X:Type*) extends ConcreteBooleanAlgebra X where
+  countable_union_mem : ∀ E : ℕ → Set X, (∀ n, measurable (E n)) → measurable (⋃ n, E n)
+
+def ConcreteSigmaAlgebra.toMeasurableSpace {X: Type*} (B: ConcreteSigmaAlgebra X) : MeasurableSpace X :=
+  by sorry
+
+def MeasurableSpace.toConcreteSigmaAlgebra {X: Type*} (M: MeasurableSpace X) : ConcreteSigmaAlgebra X :=
+  by sorry
+
+def ConcreteBooleanAlgebra.isSigmaAlgebra {X: Type*} (B: ConcreteBooleanAlgebra X) : Prop := ∀ E : ℕ → Set X, (∀ n, measurable (E n)) → measurable (⋃ n, E n)
+
+def ConcreteBooleanAlgebra.isSigmaAlgebra.toSigmaAlgebra {X: Type*} {B: ConcreteBooleanAlgebra X} (h: B.isSigmaAlgebra) : ConcreteSigmaAlgebra X :=
+  { countable_union_mem := h }
+
+/-- Exercise 1.4.10 -/
+def ConcreteBooleanAlgebra.isAtomic.isSigmaAlgebra {X: Type*} {B: ConcreteBooleanAlgebra X} (h: B.isAtomic) : B.isSigmaAlgebra :=
+  by sorry
+
+/-- Exercise 1.4.11 -/
+theorem LebesgueMeasurable.boolean_algebra.isSigmaAlgebra (d:ℕ) : (LebesgueMeasurable.boolean_algebra d).isSigmaAlgebra :=
+  by sorry
+
+def LebesgueMeasurable.sigmaAlgebra (d:ℕ) : ConcreteSigmaAlgebra (EuclideanSpace' d) :=
+  (LebesgueMeasurable.boolean_algebra.isSigmaAlgebra d).toSigmaAlgebra
+
+theorem IsNull.boolean_algebra.isSigmaAlgebra (d:ℕ) : (IsNull.boolean_algebra d).isSigmaAlgebra :=
+  by sorry
+
+def IsNull.sigmaAlgebra (d:ℕ) : ConcreteSigmaAlgebra (EuclideanSpace' d) :=
+  (IsNull.boolean_algebra.isSigmaAlgebra d).toSigmaAlgebra
+
+theorem JordanMeasurable.boolean_algebra.not_isSigmaAlgebra (d:ℕ) : ¬ (JordanMeasurable.boolean_algebra d).isSigmaAlgebra :=
+  by sorry
+
+/-- Exercise 1.4.12 -/
+theorem ConcreteSigmaAlgebra.restrict_is_sigma {X:Type*} (B: ConcreteSigmaAlgebra X) (A:Set X): (B.restrict A).isSigmaAlgebra := by sorry
+
+def ConcreteSigmaAlgebra.restrict {X:Type*} (B: ConcreteSigmaAlgebra X) (A:Set X) : ConcreteSigmaAlgebra A := (B.restrict_is_sigma A).toSigmaAlgebra
+
+
