05b6bbff9b44d9209f88193d8a954724aae5278b
Terence Tao <tao@math.ucla.edu>
2026-01-11 22:14:37 -0800

    start on 1.4.3

--- analysis/Analysis/MeasureTheory/Section_1_4_3.lean
+++ analysis/Analysis/MeasureTheory/Section_1_4_3.lean
@@ -6,3 +6,84 @@ import Analysis.MeasureTheory.Section_1_4_2
 A companion to (the introduction to) Section 1.4.3 of the book "An introduction to Measure Theory".
 
 -/
+
+/-- Definition 1.4.19 (Finitely additive measure) -/
+class FinitelyAdditiveMeasure {X:Type*} (B: ConcreteBooleanAlgebra X) where
+  measure : Set X → EReal
+  measure_pos : ∀ A : Set X, B.measurable A → 0 ≤ measure A
+  measure_empty : measure ∅ = 0
+  measure_finite_additive : ∀ E F : Set X, B.measurable E → B.measurable F → Disjoint E F →
+    measure (E ∪ F) = measure E + measure F
+
+/-- Example 1.4.21 -/
+noncomputable def FinitelyAdditiveMeasure.lebesgue (d:ℕ) : FinitelyAdditiveMeasure (LebesgueMeasurable.boolean_algebra d) :=
+  {
+    measure A := Lebesgue_measure A
+    measure_pos := by sorry
+    measure_empty := by sorry
+    measure_finite_additive := by sorry
+  }
+
+/-- Example 1.4.21 -/
+def FinitelyAdditiveMeasure.restrict_alg {X:Type*} {B: ConcreteBooleanAlgebra X} (μ: FinitelyAdditiveMeasure B) {B':ConcreteBooleanAlgebra X} (hBB': B' ≤ B) : FinitelyAdditiveMeasure B' :=
+  {
+    measure := μ.measure
+    measure_pos := by sorry
+    measure_empty := by sorry
+    measure_finite_additive := by sorry
+  }
+
+/-- Example 1.4.21 -/
+noncomputable def FinitelyAdditiveMeasure.jordan (d:ℕ) : FinitelyAdditiveMeasure (JordanMeasurable.boolean_algebra d) :=
+(FinitelyAdditiveMeasure.lebesgue d).restrict_alg (LebesgueMeasurable.gt_jordan_boolean_algebra d)
+
+/-- Example 1.4.21 -/
+noncomputable def FinitelyAdditiveMeasure.null (d:ℕ) : FinitelyAdditiveMeasure (IsNull.boolean_algebra d) :=
+(FinitelyAdditiveMeasure.lebesgue d).restrict_alg (IsNull.lt_lebesgue_boolean_algebra d)
+
+/-- Example 1.4.21 -/
+noncomputable def FinitelyAdditiveMeasure.elem (d:ℕ) : FinitelyAdditiveMeasure (EuclideanSpace'.elementary_boolean_algebra d) :=
+(FinitelyAdditiveMeasure.lebesgue d).restrict_alg (by sorry)
+
+open Classical in
+/-- Example 1.4.22 (Dirac measure) -/
+noncomputable def FinitelyAdditiveMeasure.dirac {X:Type*} (x₀:X) (B: ConcreteBooleanAlgebra X) : FinitelyAdditiveMeasure B :=
+  {
+    measure := fun A => if x₀ ∈ A then 1 else 0
+    measure_pos := by sorry
+    measure_empty := by sorry
+    measure_finite_additive := by sorry
+  }
+
+/-- Example 1.4.23 (Zero measure) -/
+def FinitelyAdditiveMeasure.zero {X:Type*} (B: ConcreteBooleanAlgebra X) : FinitelyAdditiveMeasure B :=
+  {
+    measure := fun A => 0
+    measure_pos := by sorry
+    measure_empty := by sorry
+    measure_finite_additive := by sorry
+  }
+
+/-- Example 1.4.24 (linear combinations of measures) -/
+instance FinitelyAdditiveMeasure.instAdd {X:Type*} {B: ConcreteBooleanAlgebra X} : Add (FinitelyAdditiveMeasure B) :=
+  {
+    add := fun μ ν =>
+      {
+        measure := fun A => μ.measure A + ν.measure A
+        measure_pos := by sorry
+        measure_empty := by sorry
+        measure_finite_additive := by sorry
+      }
+  }
+
+noncomputable instance FinitelyAdditiveMeasure.instSmul {X:Type*} {B: ConcreteBooleanAlgebra X} : SMul ENNReal (FinitelyAdditiveMeasure B) :=
+{
+    smul := fun c μ =>
+        {
+        measure := fun A => c * μ.measure A
+        measure_pos := by sorry
+        measure_empty := by sorry
+        measure_finite_additive := by sorry
+        }
+}
+
