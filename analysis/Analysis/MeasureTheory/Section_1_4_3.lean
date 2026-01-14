69d193f838ec8810ec2c4beae57581691837e02b
Terence Tao <tao@math.ucla.edu>
2026-01-13 22:27:30 -0800

    more

--- analysis/Analysis/MeasureTheory/Section_1_4_3.lean
+++ analysis/Analysis/MeasureTheory/Section_1_4_3.lean
@@ -144,4 +144,156 @@ open Classical in
 /-- Exercise 1.4.21 -/
 theorem FinitelyAdditiveMeasure.finite_atomic_eq {I X: Type*} [Fintype I] {atoms: I → Set X} (h_part: IsPartition atoms) (μ : FinitelyAdditiveMeasure h_part.to_ConcreteBooleanAlgebra) : ∃! c : I → ENNReal, ∀ E, h_part.to_ConcreteBooleanAlgebra.measurable E → μ.measure E = ∑ i ∈ Finset.univ.filter (fun i => atoms i ⊆ E), c i := by sorry
 
+/-- Definition 1.4.27 (Countably additive measure) -/
+class CountablyAdditiveMeasure {X:Type*} (B: ConcreteSigmaAlgebra X) extends FinitelyAdditiveMeasure B.toConcreteBooleanAlgebra where
+  measure_countable_additive : ∀ (E : ℕ → Set X), (∀ n, B.measurable (E n)) → Set.univ.PairwiseDisjoint E →
+    measure (⋃ n, E n) = ∑' n, (measure (E n))
 
+def FinitelyAdditiveMeasure.isCountablyAdditive {X:Type*} {B: ConcreteBooleanAlgebra X} (μ: FinitelyAdditiveMeasure B) : Prop :=
+  B.isSigmaAlgebra ∧ ∀ (E : ℕ → Set X), (∀ n, B.measurable (E n)) → Set.univ.PairwiseDisjoint E →
+    μ.measure (⋃ n, E n) = ∑' n, (μ.measure (E n))
+
+def FinitelyAdditiveMeasure.isCountablyAdditive.toCountablyAdditive {X:Type*} {B: ConcreteBooleanAlgebra X} (μ: FinitelyAdditiveMeasure B) (h: μ.isCountablyAdditive) : CountablyAdditiveMeasure h.1.toSigmaAlgebra :=
+  {
+    measure := μ.measure
+    measure_pos := μ.measure_pos
+    measure_empty := μ.measure_empty
+    measure_finite_additive := μ.measure_finite_additive
+    measure_countable_additive := h.2
+  }
+
+/-- Example 1.4.28-/
+theorem FinitelyAdditiveMeasure.lebesgue_isCountablyAdditive (d:ℕ) : (FinitelyAdditiveMeasure.lebesgue d).isCountablyAdditive :=
+  by sorry
+
+theorem FinitelyAdditiveMeasure.isCountablyAdditive_restrict_alg {X:Type*} {B B': ConcreteSigmaAlgebra X} (μ: CountablyAdditiveMeasure B) (hBB': B' ≤ B) : (μ.toFinitelyAdditiveMeasure.restrict_alg hBB').isCountablyAdditive :=
+  by sorry
+
+def CountablyAdditiveMeasure.restrict_alg {X:Type*} {B B': ConcreteSigmaAlgebra X} (μ: CountablyAdditiveMeasure B) (hBB' : B' ≤ B) : CountablyAdditiveMeasure B' :=
+  {
+    toFinitelyAdditiveMeasure := μ.toFinitelyAdditiveMeasure.restrict_alg hBB',
+    measure_countable_additive := by sorry
+  }
+
+/-- Example 1.4.29-/
+theorem FinitelyAdditiveMeasure.dirac_isCountablyAdditive {X:Type*} (x₀:X) (B: ConcreteBooleanAlgebra X) : (FinitelyAdditiveMeasure.dirac x₀ B).isCountablyAdditive :=
+  by sorry
+
+/-- Example 1.4.29-/
+theorem FinitelyAdditiveMeasure.counting_isCountablyAdditive {X:Type*} : (FinitelyAdditiveMeasure.counting X).isCountablyAdditive :=
+  by sorry
+
+/-- Example 1.4.30 -/
+def CountablyAdditiveMeasure.restrict {X:Type*} {B: ConcreteSigmaAlgebra X} (μ: CountablyAdditiveMeasure B) (A:Set X) (hA:B.measurable A) : CountablyAdditiveMeasure (B.restrict A) :=
+  {
+    toFinitelyAdditiveMeasure := μ.toFinitelyAdditiveMeasure.restrict A hA,
+    measure_countable_additive := by sorry
+  }
+
+instance CountablyAdditiveMeasure.instZero {X:Type*} (B: ConcreteSigmaAlgebra X) : Zero (CountablyAdditiveMeasure B) :=
+  {
+    zero := {
+      toFinitelyAdditiveMeasure := 0
+      measure_countable_additive := by sorry
+    }
+  }
+
+instance CountablyAdditiveMeasure.instAdd {X:Type*} {B: ConcreteSigmaAlgebra X} : Add (CountablyAdditiveMeasure B) :=
+  {
+    add := fun μ ν =>
+      {
+        toFinitelyAdditiveMeasure := μ.toFinitelyAdditiveMeasure + ν.toFinitelyAdditiveMeasure
+        measure_countable_additive := by sorry
+      }
+  }
+
+instance CountablyAdditiveMeasure.instAddCommMonoid {X:Type*} {B: ConcreteSigmaAlgebra X} : AddCommMonoid (CountablyAdditiveMeasure B) :=
+{
+  add_assoc := by sorry,
+  zero_add := by sorry,
+  add_zero := by sorry,
+  add_comm := by sorry
+  nsmul := nsmulRec
+}
+
+/-- Exercise 1.4.22(i) -/
+noncomputable instance CountablyAdditiveMeasure.instSmul {X:Type*} {B: ConcreteSigmaAlgebra X} : SMul ENNReal (CountablyAdditiveMeasure B) :=
+{
+    smul := fun c μ =>
+        {
+        toFinitelyAdditiveMeasure := c • μ.toFinitelyAdditiveMeasure
+        measure_countable_additive := by sorry
+        }
+}
+
+noncomputable instance CountablyAdditiveMeasure.instDistribMulAction {X:Type*} {B: ConcreteSigmaAlgebra X} : DistribMulAction ENNReal (CountablyAdditiveMeasure B) :=
+{
+  smul_zero := by sorry,
+  smul_add := by sorry,
+  one_smul := by sorry,
+  mul_smul := by sorry
+}
+
+/-- Exercise 1.4.22(ii) -/
+noncomputable def CountablyAdditiveMeasure.sum {X:Type*} {B: ConcreteSigmaAlgebra X} (μ: ℕ → CountablyAdditiveMeasure B) : CountablyAdditiveMeasure B :=
+  {
+    toFinitelyAdditiveMeasure := {
+      measure := fun A => ∑' n, (μ n).toFinitelyAdditiveMeasure.measure A
+      measure_pos := by sorry
+      measure_empty := by sorry
+      measure_finite_additive := by sorry
+    }
+    measure_countable_additive := by sorry
+  }
+
+open MeasureTheory
+
+noncomputable def CountablyAdditiveMeasure.toMeasure {X:Type*} {B: ConcreteSigmaAlgebra X} (μ: CountablyAdditiveMeasure B) :
+  @Measure X B.measurableSpace :=
+  let _measurable := B.measurableSpace
+  {
+      measureOf E := (μ.measure E).toENNReal
+      empty := by sorry
+      mono := by sorry
+      iUnion_nat := by sorry
+      m_iUnion := by sorry
+      trim_le := by sorry
+  }
+
+def Measure.toCountablyAdditiveMeasure {X:Type*} [M : MeasurableSpace X] (μ: Measure X) : CountablyAdditiveMeasure M.sigmaAlgebra :=
+  {
+    toFinitelyAdditiveMeasure := {
+      measure E := μ.measureOf E
+      measure_pos := by sorry
+      measure_empty := by sorry
+      measure_finite_additive := by sorry
+    }
+    measure_countable_additive := by sorry
+  }
+
+/-- Exercise 1.4.23(i) -/
+theorem Measure.countable_subadditivity {X:Type*} [MeasurableSpace X] (μ: Measure X) {E : ℕ → Set X} (hE: ∀ n, Measurable (E n)) :
+  μ.measureOf (⋃ n, E n) ≤ ∑' n, μ.measureOf (E n) := by sorry
+
+/-- Exercise 1.4.23(ii) -/
+theorem Measure.upwards_mono {X:Type*} [MeasurableSpace X] (μ: Measure X) {E : ℕ → Set X} (hE: ∀ n, Measurable (E n))
+  (hmono : Monotone E) : μ (⋃ n, E n) = ⨆ n, μ.measureOf (E n) := by sorry
+
+/-- Exercise 1.4.23(iii) -/
+theorem Measure.downwards_mono {X:Type*} [MeasurableSpace X] (μ: Measure X) {E : ℕ → Set X} (hE: ∀ n, Measurable (E n))
+  (hmono : Antitone E) (hfin : ∃ n, μ (E n) < ⊤) : μ (⋂ n, E n) = ⨅ n, μ.measureOf (E n) := by sorry
+
+theorem Measure.downwards_mono_counter : ∃ (X:Type) (M: MeasurableSpace X) (μ: Measure X) (E : ℕ → Set X) (hE: ∀ n, Measurable (E n))
+  (hmono : Antitone E), μ (⋂ n, E n) ≠ ⨅ n, μ.measureOf (E n) := by sorry
+
+/-- Exercise 1.4.24 (a) (Dominated convergence for sets) -/
+theorem Measure.measurable_of_lim {X:Type*} [MeasurableSpace X] (μ: Measure X) {E : ℕ → Set X} (hE: ∀ n, Measurable (E n))
+  {E' : Set X} (hlim : PointwiseConvergesTo E E') : Measurable E := by sorry
+
+theorem Measure.measure_of_lim {X:Type*} [MeasurableSpace X] (μ: Measure X) {E : ℕ → Set X} (hE: ∀ n, Measurable (E n))
+  {E' F : Set X} (hlim : PointwiseConvergesTo E E') (hF : Measurable F) (hfin : μ F < ⊤) (hcon : ∀ n, E n ⊆ F) :
+  Filter.atTop.Tendsto (fun n ↦ μ (E n)) (nhds (μ E')) := by sorry
+
+theorem Measure.measure_of_lim_counter : ∃ (X:Type) (M:MeasurableSpace X) (μ: Measure X) (E : ℕ → Set X) (hE: ∀ n, Measurable (E n))
+  (E' F : Set X) (hlim : PointwiseConvergesTo E E') (hF : Measurable F) (hcon : ∀ n, E n ⊆ F),
+  ¬ Filter.atTop.Tendsto (fun n ↦ μ (E n)) (nhds (μ E')) := by sorry
